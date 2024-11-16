c7497b9395c6d1059f9dbe3f239b096285ae2a9d
Saptarshi Mondal <saptarshimondal.mail@gmail.com>
2024-11-16 21:07:24 +0530

    JS file added and new code added

---
+++ script.js
@@ -0,0 +1,9 @@
+let inputField = document.querySelector(".inputField");
+const btnTab = document.querySelector(".btnTab");
+
+while (btnTab.onclick) {
+    if(inputField === " "){
+        alert("Please enter a word");
+    } else {
+        
+    }
