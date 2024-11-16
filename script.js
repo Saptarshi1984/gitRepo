e02e28e35d282508ed6a6137da109167b9ca0980
Saptarshi Mondal <saptarshimondal.mail@gmail.com>
2024-11-16 21:23:47 +0530

    New styles added

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
 let inputField = document.querySelector(".inputField");
 const btnTab = document.querySelector(".btnTab");
 
-while (btnTab.onclick) {
-    if(inputField === " "){
-        alert("Please enter a word");
-    } else {
-        
-    }
